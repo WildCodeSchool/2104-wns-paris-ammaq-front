319240fa25578b0fc3186678daf3237f4ce83820
Alicia Pilar <aliciapilar@Air-de-Alicia.home>
2021-04-28 14:25:20 +0200

    removing spaces

--- src/components/UserNav/index.tsx
+++ src/components/UserNav/index.tsx
@@ -12,7 +12,7 @@ const UserNav = (): JSX.Element => {
     <div
       id="user-nav"
       className={`rounded mt-2 mr-3 flex flex-col flex-shrink-0 bg-usersnav absolute inset-y-0 right-0 shadow-usersnav ${
-        close ? " p-4" : "  w-1/12"
+        close ? "p-4" : "w-1/12"
       }`}
     >
       {
@@ -38,9 +38,7 @@ const UserNav = (): JSX.Element => {
         // users
       }
 
-      <div
-        className={`mt-5 ml-3 grid grid-flow-col ${close ? " pt-2" : "  p-2"}`}
-      >
+      <div className={`mt-5 ml-3 grid grid-flow-col ${close ? "pt-2" : "p-2"}`}>
         <img
           className="user-nav-img rounded-full object-cover z-10"
           src="https://cqfd.univ-lyon1.fr/files/2020/04/3-pieuvre-gif.gif"
@@ -55,9 +53,7 @@ const UserNav = (): JSX.Element => {
         </p>
       </div>
 
-      <div
-        className={`mt-5 ml-3 grid grid-flow-col ${close ? " pt-2" : "  p-2"}`}
-      >
+      <div className={`mt-5 ml-3 grid grid-flow-col ${close ? "pt-2" : "p-2"}`}>
         <img
           className="user-nav-img rounded-full object-cover z-10"
           src="https://www.lebernard.ca/wp-content/uploads/2015/09/Chien-chinois-a-crete.jpg"
@@ -65,7 +61,7 @@ const UserNav = (): JSX.Element => {
         />
         <p
           className={`uppercase text-xs text-gray-300 items-end truncate ml-3 self-center ${
-            close ? " hidden" : ""
+            close ? "hidden" : ""
           }`}
         >
           Chien Chinois à Crête
