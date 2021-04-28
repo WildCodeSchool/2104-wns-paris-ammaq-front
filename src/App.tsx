965508f778cdc7e3d59c97d5d1bedfb702f76ef9
Alan <tr08td3@protonmail.ch>
2021-04-28 14:17:55 +0200

    Removed <Meet> from App()

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,24 @@
 import React from "react";
 import "./App.css";
-import Meet from "./components/Meet";
 
 function App(): JSX.Element {
   return (
-    <Meet
-      parentNode="jitsi-container"
-      roomName="ammaq"
-      displayName="ammaq"
-      subject="jitsi"
-      width={window.innerWidth}
-      height={window.innerHeight}
-    />
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
