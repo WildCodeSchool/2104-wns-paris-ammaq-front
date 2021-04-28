81b56604a6b3847d6c7df13285490cb0b0ee3ae0
Mohamed M'rabet <mhmdmrabet@yahoo.fr>
2021-04-28 14:43:06 +0200

    Ajout channel nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import ChannelNav from "./components/ChannelNav";
 
-function App(): JSX.Element {
+export default function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChannelNav />
     </div>
   );
 }
-
-export default App;
