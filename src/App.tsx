73e33c60407b9e0b48e1683afa19e4ad64b2e6e8
Novali <alicia.pilar83@gmail.com>
2021-04-29 16:25:18 +0200

    Revert "Dev"

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-
 import UserNav from "./components/UserNav/index";
 import MainNav from "./components/MainNav";
 import "./App.css";
-import ChannelNav from "./components/ChannelNav";
 
-export default function App(): JSX.Element {
+function App(): JSX.Element {
   return (
     <div className="App">
-      <ChannelNav />
       <UserNav />
       <MainNav />
-
     </div>
   );
 }
+
+export default App;
