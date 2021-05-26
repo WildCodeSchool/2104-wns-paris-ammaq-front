8fbf766ceea6229976dccc38bca7b91103dc79bb
wazo <manon.gerray@gmail.com>
2021-05-26 17:03:28 +0200

    :blue_car: main nav routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import MainNav from "./components/MainNav";
-import Community from "./components/Community";
+import Page from "./components/Page";
 import "./App.css";
 
 export default function App(): JSX.Element {
   return (
     <div className="App bg-main-darkgrey flex">
       <MainNav />
-      <Community />
+      <Page />
     </div>
   );
 }
