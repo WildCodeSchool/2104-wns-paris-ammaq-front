bd40fe2dc639ea03ceee23496248b062958c5ca7
Mohamed M'rabet <mhmdmrabet@yahoo.fr>
2021-05-27 16:36:06 +0200

    Add test

--- src/components/Base/UserSmallCard.tsx
+++ src/components/Base/UserSmallCard.tsx
@@ -18,7 +18,10 @@ const UserSmallCard = ({
   lastname,
 }: IUserProps): JSX.Element => {
   return (
-    <div className={`mt-5 ml-3 grid grid-flow-col ${close ? "pt-2" : "p-2"}`}>
+    <div
+      data-testid="userSmallCard"
+      className={`mt-5 ml-3 grid grid-flow-col ${close ? "pt-2" : "p-2"}`}
+    >
       <img
         className="user-nav-img rounded-full object-cover z-10"
         src="https://cqfd.univ-lyon1.fr/files/2020/04/3-pieuvre-gif.gif"
