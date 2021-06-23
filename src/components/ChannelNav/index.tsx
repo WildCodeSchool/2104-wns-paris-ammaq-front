f6aaabd2237f5024499f4da505588271edc69ae7
Alicia Pilar <aliciapilar@alicias-macbook-air.home>
2021-06-23 12:20:46 +0200

    channel loading

--- src/components/ChannelNav/index.tsx
+++ src/components/ChannelNav/index.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
 import Header from "./Header";
 import Channel from "./Channel";
+import ChannelLoading from "./ChannelLoading";
 import ChannelType from "../../types/Channel";
+import { ChannelsQuery } from "../../graphql/queries/channel";
 
 type ChannelNavProps = {
   channels: ChannelType[];
@@ -11,6 +14,8 @@ type ChannelNavProps = {
 
 const ChannelNav = ({ channels }: ChannelNavProps): JSX.Element => {
   const [activeIndex, setActiveIndex] = useState(0);
+
+  const { data, loading, error } = useQuery(ChannelsQuery);
 
   const handleClick = (index: number) => {
     setActiveIndex(index);
@@ -20,6 +25,13 @@ const ChannelNav = ({ channels }: ChannelNavProps): JSX.Element => {
     <div className="w-2/12 min-h-screen bg-main-darkgrey p-2 text-xs">
       <Header />
       <div data-testid="list-channels">
+        {loading ? (
+          <>
+            <ChannelLoading />
+            <ChannelLoading />
+            <ChannelLoading />
+          </>
+        ) : null}
         {channels.map((element: ChannelType, index: number) => {
           const isActive = activeIndex === index;
           return (
