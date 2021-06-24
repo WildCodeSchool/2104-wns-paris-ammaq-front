d382ed6e8a4de8ddf895481b28c90c45a385f170
Quentin Liger <qliger.dev@gmail.com>
2021-06-24 16:13:53 +0200

    updated Jutsu component to reload on room switch

--- src/components/Community.tsx
+++ src/components/Community.tsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
+import { Jutsu } from "../utils/Jutsu";
 import UserNav from "./UserNav/index";
 import ChannelNav from "./ChannelNav";
-import Meet from "./Meet";
-import ChannelType from "../types/Channel";
 import { ChannelsQuery } from "../graphql/queries/channel";
+
+function Loader(): JSX.Element {
+  return <p className="text-white">Loading</p>;
+}
 
 export default function Community(): JSX.Element {
   const { loading, error, data } = useQuery(ChannelsQuery);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <div className=" flex w-screen">
-      <ChannelNav channels={channels} />
+    <div className="w-screen flex">
+      <ChannelNav
+        channels={data?.channels}
+        loading={loading}
+        error={error}
+        active={activeIndex}
+        handleSwitch={(index) => setActiveIndex(index)}
+      />
       <div className="flex-1">
-        <Meet
-          parentNode="jitsy-container"
-          roomName="WORKIT-cours-karim"
-          displayName="Quentin"
-          subject="cours-karim"
-          height={window.innerHeight}
-        />
+        {data && (
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          <Jutsu
+            roomName={`WORKIT-${data?.channels[activeIndex].name}`}
+            subject={data?.channels[activeIndex].name}
+            displayName="Quentin"
+            height={window.innerHeight}
+            loadingComponent={Loader}
+          />
+        )}
       </div>
       <UserNav />
     </div>
