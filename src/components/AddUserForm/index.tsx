9d3931e2f89c0876e120ea81c32bf12b5be607b2
Mohamed <mhmdmrabet@yahoo.fr>
2021-07-27 14:49:19 +0200

    Added form user

--- src/components/AddUserForm/index.tsx
+++ src/components/AddUserForm/index.tsx
@@ -4,12 +4,15 @@ import React from "react";
 import { Path, useForm, SubmitHandler, UseFormRegister } from "react-hook-form";
 import { joiResolver } from "@hookform/resolvers/joi";
 import Joi from "joi";
+import { useMutation } from "@apollo/client";
+import { CreateUser } from "../../graphql/mutations/user";
 
 type IFormValues = {
   email: string;
   firstname: string;
   lastname: string;
   avatar: string;
+  password: string;
 };
 
 type InputProps = {
@@ -22,9 +25,10 @@ const schema = Joi.object({
   email: Joi.string()
     .email({ tlds: { allow: false } })
     .required(),
-  firstname: Joi.string().required(),
-  lastname: Joi.string().required(),
-  avatar: Joi.string(),
+  firstname: Joi.string().required().trim(),
+  lastname: Joi.string().required().trim(),
+  avatar: Joi.string().trim().optional().allow(""),
+  password: Joi.string().required().trim(),
 });
 
 const Input = ({ label, register, required }: InputProps) => (
@@ -32,36 +36,48 @@ const Input = ({ label, register, required }: InputProps) => (
     <label className="text-white">{label}</label>
     <input
       {...register(label, { required })}
-      className="border-none rounded px-3 py-2 w-full focus:outline-none focus:ring-main-red focus:ring-2 focus:shadow bg-pressed shadow-channels"
+      className="text-white border-none rounded px-3 py-2 w-full focus:outline-none focus:ring-main-red focus:ring-2 focus:shadow bg-pressed shadow-channels"
     />
   </div>
 );
 
-const onSubmit: SubmitHandler<IFormValues> = (data) => console.log(data);
-
 const AddUser = (): JSX.Element => {
+  const [addUser] = useMutation(CreateUser);
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<IFormValues>({ resolver: joiResolver(schema) });
 
+  const onSubmit: SubmitHandler<IFormValues> = (input) => {
+    // eslint-disable-next-line object-shorthand
+    addUser({ variables: { input: input } });
+    reset();
+  };
   return (
     <div className="w-1/2 mx-auto mt-5 shadow-mainnav p-4">
       <form className="" onSubmit={handleSubmit(onSubmit)}>
         <Input label="email" register={register} required />
         {errors.email && (
-          <span className="text-red-500">Email is required</span>
+          <span className="text-red-500">{errors.email.message}</span>
         )}
         <Input label="firstname" register={register} required />
         {errors.firstname && (
-          <span className="text-red-500">First name is required</span>
+          <span className="text-red-500">{errors.firstname.message}</span>
         )}
         <Input label="lastname" register={register} required />
         {errors.lastname && (
-          <span className="text-red-500">Last name is required</span>
+          <span className="text-red-500">{errors.lastname.message}</span>
         )}
         <Input label="avatar" register={register} required={false} />
+        {errors.avatar && (
+          <span className="text-red-500">{errors.avatar.message}</span>
+        )}
+        <Input label="password" register={register} required={false} />
+        {errors.avatar && (
+          <span className="text-red-500">{errors.password?.message}</span>
+        )}
         <div className="text-center">
           <input
             type="submit"
