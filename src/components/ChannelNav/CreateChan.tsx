513ea90f144db24bf1305c661cf9bf7e68a6aa6c
wazo <manon.gerray@gmail.com>
2021-07-29 09:19:12 +0200

    :lipstick: toggle isVocal or not

--- src/components/ChannelNav/CreateChan.tsx
+++ src/components/ChannelNav/CreateChan.tsx
@@ -4,87 +4,105 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useMutation } from "@apollo/client";
-import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { Edit2, Video } from "react-feather";
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { joiResolver } from "@hookform/resolvers/joi";
+import Joi from "joi";
 
 import { XCircle } from "react-feather";
 import { CreateChannel } from "../../graphql/mutations/channel";
+
+import "./channels.css";
 
 type CreateChanProps = {
   closeModal: any;
 };
 
+type FormValues = {
+  name: string;
+  isVocal: boolean;
+};
+
+const schema = Joi.object({
+  name: Joi.string().required(),
+  isVocal: Joi.boolean().required(),
+});
+
 const CreateChan = ({ closeModal }: CreateChanProps): JSX.Element => {
-  const [chanName, setChanName] = useState("");
-  const [chanType, setChanType] = useState(true);
   const [createChannel] = useMutation(CreateChannel);
+  const [channelName, setChannelName] = useState("");
+  const [checked, setChecked] = useState(true);
 
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
-  } = useForm();
+  } = useForm<FormValues>({ resolver: joiResolver(schema) });
 
-  const onSubmit = (data: any) =>
-    createChannel({
-      variables: {
-        data,
-      },
-    });
-
-  const handleChanName = (event: {
-    target: { value: React.SetStateAction<string> };
-  }) => {
-    setChanName(event.target.value);
+  const onSubmit: SubmitHandler<FormValues> = (input) => {
+    createChannel({ variables: { input } });
+    reset();
+    closeModal();
   };
-
-  const handleChanType = (event: {
-    target: { value: React.SetStateAction<string> };
-  }) => {
-    console.log(event.target.value);
-    event.target.value === "video" ? setChanType(false) : setChanType(true);
-  };
-
-  useEffect(() => {
-    console.log(chanType);
-  });
 
   return (
-    <div className="absolute z-10 w-80 h-56 left-1/4 bg-main-darkgrey rounded-2xl p-4 text-main-white border-community-green-light border-solid border-2">
+    <div
+      className={`absolute z-10 w-80 h-56 left-1/4 bg-main-darkgrey rounded-2xl p-4 text-main-white border-solid border-2 ${
+        checked ? "border-community-green-light" : "border-community-blue"
+      }`}
+    >
       <button type="button" onClick={closeModal} className="ml-64">
         <XCircle color="red" />
       </button>
       <div>
         <h1 className="text-center text-base">Créer un nouveau channel</h1>
       </div>
-      <form className="p-2 flex flex-col">
+      <form className="p-2 flex flex-col" onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
-          placeholder="nom-du-nouveau-channel"
-          className="p-2 bg-main-darkgrey focus:outline-none border-solid border border-main-darkgrey focus:border-community-green-light"
           {...register("name")}
+          onChange={(e: any) => setChannelName(e.currentTarget.value)}
+          required
         />
+        {errors.name && (
+          <span className="text-red-500">{errors.name.message}</span>
+        )}
         <div className="mt-2">
           <p>
-            <span className="text-community-green-light">{register.name}</span>
+            <span className={`${checked ? "text-community-green-light" : "text-community-blue"`}>{channelName} </span>
             est un channel audio ou vidéo ?
           </p>
-          <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
-            <input
-              type="checkbox"
-              id="toggle"
-              className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
-              {...register("isVocal")}
-            />
+
+          <div className="flex items-center justify-center w-full mb-12">
             <label
-              htmlFor="toggle"
-              className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
-            />
+              htmlFor="toggleB"
+              className="flex items-center cursor-pointer"
+            >
+              <div className="relative toggle-checkbox">
+                <input
+                  id="toggleB"
+                  type="checkbox"
+                  className="sr-only toggle-checkbox cursor-pointer"
+                  {...register("isVocal")}
+                  onChange={() => setChecked(!checked)}
+                />
+                <div className="block bg-gray-700 w-14 h-8 rounded-full" />
+                {checked ? (
+                  <Edit2 className="absolute right-2 top-1 w-4 text-community-green-light" />
+                ) : (
+                  <Video className="absolute left-2 top-1 w-4 text-community-blue" />
+                )}
+                <div className="dot absolute left-1 top-1 w-6 h-6 rounded-full transition" />
+                {errors.isVocal && (
+                  <span className="text-red-500">{errors.isVocal.message}</span>
+                )}
+              </div>
+            </label>
           </div>
         </div>
-        <button type="submit" onSubmit={handleSubmit(onSubmit)}>
-          Créer
-        </button>
+        <button type="submit"> Créer </button>
       </form>
     </div>
   );
