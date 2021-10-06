ae3beca0f6a37402d62ecb60ef78451642bd837f
wazo <manon.gerray@gmail.com>
2021-10-06 15:08:33 +0200

    clean imports

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { Redirect } from "react-router";
 
 import Community from "./Community";
 import Agenda from "./Agenda";
