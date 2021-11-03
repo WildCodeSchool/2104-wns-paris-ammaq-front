338509c78ac940f873bafd6bdd756d2de1ea33e1
wazo <manon.gerray@gmail.com>
2021-11-03 16:21:29 +0100

    Deleting unused function

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -5,11 +5,3 @@ export const firstLetter = (name) => {
 export const withoutFirst = (name) => {
   return name.substring(1);
 };
-
-export const compactChan = (chan) => {
-  console.log(chan);
-  const compacted = chan.split(" ");
-  console.log(compacted);
-  console.log(compacted.join(""));
-  return compacted.join("");
-};
