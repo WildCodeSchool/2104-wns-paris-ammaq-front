94397d82620f678a13f4597f71fdb4ad964a7345
wazo <manon.gerray@gmail.com>
2021-12-07 11:43:28 +0100

    Display current time & user firstname

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,30 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../context/auth-provider";
 
 import { getMonthName } from "../utils/timeFunctions";
 
 const Dashboard = (): JSX.Element => {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  const { token } = useAuth();
+
   const today = new Date();
-  const currentTime = () => {
-    return today.toLocaleTimeString();
-  };
-  const timePlaying = setInterval(currentTime, 1000);
   const currentDate = `${today.getDate()} ${getMonthName(
     today
   )} ${today.getFullYear()}`;
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div>
       <h1>
-        Hello <span>Alicia</span>
+        Bonjour <span>{token?.firstname}</span>
       </h1>
       <div>
-        <p>
-          Aujourd&rsquo;hui tu as <span>2</span> cours de prévus
-        </p>
-        <div>
-          <h3>{currentDate}</h3>
-          <h3>{timePlaying}</h3>
-        </div>
+        <h3>
+          Nous sommes le <span>{currentDate}</span>
+        </h3>
+        <h3>
+          <span>{time}</span>
+        </h3>
       </div>
     </div>
   );
