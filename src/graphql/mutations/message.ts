8c443fddf43db5e5f4ec258e6cbcc5d26bcd0c71
n0vali <alicia.pilar83@gmail.com>
2021-12-15 10:51:55 +0100

    works

--- src/graphql/mutations/message.ts
+++ src/graphql/mutations/message.ts
@@ -33,9 +33,7 @@ export const NEWMESSAGE_SUBSCRIPTION = gql`
 `;
 
 export const DELETE_SUBSCRIPTION = gql`
-  subscription OnMessageDeleted($id: ID!) {
-    deletedMessage(id: $id) {
-      id
-    }
+  subscription OnMessageDeleted {
+    deletedMessage
   }
 `;
